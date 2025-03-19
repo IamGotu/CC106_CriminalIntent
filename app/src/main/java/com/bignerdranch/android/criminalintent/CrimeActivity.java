f83443814d9131721943715eb7cd93ae6a776d54
Mark John <markjohnjopia1@gmail.com>
2025-03-19 18:32:51 +0800

    Renaming MainActivity.java to CrimeActivity.java

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -4,13 +4,10 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+public class CrimeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
