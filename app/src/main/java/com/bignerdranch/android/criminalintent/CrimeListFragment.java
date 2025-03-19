b8b45ffbf1fda0154f77276b513b8f06573e9e6c
Mark John <markjohnjopia1@gmail.com>
2025-03-19 19:28:42 +0800

    Chapter 8  Displaying Lists with RecyclerView: A typical ViewHolder subclass

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -1,8 +1,19 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class CrimeListFragment extends Fragment {
 
-    // Nothing yet
+    public class ListRow extends RecyclerView.ViewHolder {
+        public ImageView mThumbnail;
+        public ListRow(View view) {
+            super(view);
+            mThumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        }
+    }
+
 }
