0fceabc8c60d855289d335931b36508a3df59348
Mark John <markjohnjopia1@gmail.com>
2025-03-19 17:00:21 +0800

    Chapter 7  UI Fragments and the Fragment Manager: Overriding Fragment.onCreate(Bundle)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,10 +1,21 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
 
 
 }
