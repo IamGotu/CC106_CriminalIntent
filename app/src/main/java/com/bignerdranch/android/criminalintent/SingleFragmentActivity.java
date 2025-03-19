26a532f27815c30c17184e6ec0eda61fc47eaeeb
Mark John <markjohnjopia1@gmail.com>
2025-03-19 19:01:06 +0800

    Status bar icons will stay visible in both light and dark mode

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,10 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsetsController;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -22,5 +26,33 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+
+        Window window = getWindow();
+        View decorView = window.getDecorView();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // For Android 11+ (API 30+)
+            WindowInsetsController controller = window.getInsetsController();
+            if (controller != null) {
+                controller.setSystemBarsAppearance(
+                        isDarkModeEnabled() ? 0 : WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                );
+            }
+        } else {
+            // For older versions
+            int flags = decorView.getSystemUiVisibility();
+            if (!isDarkModeEnabled()) {
+                flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;  // Light mode (black icons)
+            } else {
+                flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR; // Dark mode (white icons)
+            }
+            decorView.setSystemUiVisibility(flags);
+        }
+    }
+
+    private boolean isDarkModeEnabled() {
+        int nightModeFlags = getResources().getConfiguration().uiMode & android.content.res.Configuration.UI_MODE_NIGHT_MASK;
+        return nightModeFlags == android.content.res.Configuration.UI_MODE_NIGHT_YES;
     }
 }
