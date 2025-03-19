56aba80d5d20e2344a43ca4da5d0930cb45321b9
Mark John <markjohnjopia1@gmail.com>
2025-03-19 18:30:43 +0800

    Chapter 8  Displaying Lists with RecyclerView: Updating layout file for MainActivity (MainActivity.java)

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
