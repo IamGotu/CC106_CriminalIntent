394abf3158525e69b854d110fdc7f0d039ea7624
Mark John <markjohnjopia1@gmail.com>
2025-03-19 17:10:32 +0800

    Chapter 7  UI Fragments and the Fragment Manager: Getting the FragmentManager (CrimeActivity.java)

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
     }
 }
